34f1b9812b78574d1d5732b2c5fa050d213c226e
Tariqul Huda <tariqulhuda6@gmail.com>
2024-09-19 00:03:56 +0600

    changed model

--- AnH/AnH/settings.py
+++ AnH/AnH/settings.py
@@ -132,6 +132,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5000",
     "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://localhost:5000",
 ]
 
 CORS_ALLOW_HEADERS = [
--- AnH/home/models.py
+++ AnH/home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class Category(models.Model):
 
     category_name = models.CharField(max_length=50)
-    def str(self):
+    def __str__(self):
         return self.category_name
 
 class Product(models.Model):
@@ -13,6 +13,6 @@ class Product(models.Model):
     product_image_url = models.CharField(help_text="Image URL of a product",null = False,max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    def str(self):
+    def __str__(self):
         return self.product_name
 
